4cba90644a464ee78d6045d594495469445a0a42
oldchen <772565130@qq.com>
2019-06-30 11:17:20 +0800

    稍微完善动画逻辑，返回逻辑

--- lib/items/task_detail_item.dart
+++ lib/items/task_detail_item.dart
@@ -3,17 +3,22 @@ import 'package:flutter/material.dart';
 class TaskDetailItem extends StatefulWidget {
   final double itemProgress;
   final Function onChecked;
+  final TheProgress onProgressChanged;
   final String itemName;
   final int index;
 
   TaskDetailItem(
-      {this.itemProgress = 0.0, this.onChecked, @required this.itemName, this.index = 0});
+      {this.itemProgress = 0.0,
+      this.onChecked,
+      @required this.itemName,
+      this.index = 0, this.onProgressChanged});
 
   @override
   _TaskDetailItemState createState() => _TaskDetailItemState();
 }
 
-class _TaskDetailItemState extends State<TaskDetailItem> with SingleTickerProviderStateMixin{
+class _TaskDetailItemState extends State<TaskDetailItem>
+    with SingleTickerProviderStateMixin {
   double currentProgress = 0.0;
   bool progressShow = false;
 
@@ -24,13 +29,17 @@ class _TaskDetailItemState extends State<TaskDetailItem> with SingleTickerProvid
   void initState() {
     super.initState();
     currentProgress = widget.itemProgress;
-    _controller = AnimationController(vsync: this,duration: Duration(milliseconds: 1000));
-    _animation = Tween(begin: 0.0,end: 1.0).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOutBack));
-    Future.delayed(Duration(seconds: 1,), (){
+    _controller = AnimationController(
+        vsync: this, duration: Duration(milliseconds: 1000));
+    _animation = Tween(begin: 0.0, end: 1.0).animate(
+        CurvedAnimation(parent: _controller, curve: Curves.easeOutBack));
+    Future.delayed(
+        Duration(
+          seconds: 1,
+        ), () {
       _controller.forward();
     });
   }
-
 
   @override
   void dispose() {
@@ -40,13 +49,16 @@ class _TaskDetailItemState extends State<TaskDetailItem> with SingleTickerProvid
 
   @override
   Widget build(BuildContext context) {
-
     final size = MediaQuery.of(context).size;
 
     return AnimatedBuilder(
       animation: _animation,
-      builder: (ctx, child){
-        return Transform.translate(offset: Offset((size.width + widget.index * 100) * (_animation.value - 1), 0), child: child,);
+      builder: (ctx, child) {
+        return Transform.translate(
+          offset: Offset(
+              (size.width + widget.index * 100) * (_animation.value - 1), 0),
+          child: child,
+        );
       },
       child: Container(
         child: Column(
@@ -63,7 +75,7 @@ class _TaskDetailItemState extends State<TaskDetailItem> with SingleTickerProvid
                         widget.onChecked(value, currentProgress);
                       }
                       setState(() {
-                        if (value == true) {
+                        if (value) {
                           currentProgress = 1.0;
                         } else {
                           currentProgress = widget.itemProgress;
@@ -75,17 +87,36 @@ class _TaskDetailItemState extends State<TaskDetailItem> with SingleTickerProvid
                 ),
                 Expanded(
                     flex: 8,
-                    child: Container(
-                        margin: EdgeInsets.only(left: 5),
-                        child: Text("${widget.itemName}"))),
+                    child: InkWell(
+                      onTap: () {
+                        setState(() {
+                          if (currentProgress == 1.0) {
+                            currentProgress = widget.itemProgress;
+                          } else {
+                            currentProgress = 1.0;
+                          }
+                        });
+                      },
+                      child: Container(
+                          margin: EdgeInsets.only(
+                            left: 5,
+                          ),
+                          child: Text("${widget.itemName}")),
+                    )),
                 Expanded(
                     flex: 1,
                     child: IconButton(
-                        icon: Icon(progressShow?Icons.arrow_drop_up:Icons.arrow_drop_down,color: Colors.grey,), onPressed: (){
+                        icon: Icon(
+                          progressShow
+                              ? Icons.arrow_drop_up
+                              : Icons.arrow_drop_down,
+                          color: Colors.grey,
+                        ),
+                        onPressed: () {
                           setState(() {
                             progressShow = !progressShow;
                           });
-                    }))
+                        }))
               ],
             ),
             progressShow ? getProgressWidget(context) : SizedBox(),
@@ -97,23 +128,37 @@ class _TaskDetailItemState extends State<TaskDetailItem> with SingleTickerProvid
 
   Row getProgressWidget(BuildContext context) {
     return Row(
-          children: <Widget>[
-            Expanded(
-              flex: 6,
-              child: Container(
-                margin: EdgeInsets.only(left: 40),
-                height: 5,
-                child: LinearProgressIndicator(
-                  valueColor: AlwaysStoppedAnimation(
-                      Theme.of(context).primaryColor),
-                  value: currentProgress,
-                  backgroundColor: Color.fromRGBO(224, 224, 224, 1),
-                ),
-              ),
-            ),
-            SizedBox(width: 20,),
-            Expanded(flex: 2,child: Text("${(currentProgress * 100).toInt()}%",style: TextStyle(fontSize: 8),),),
-          ],
-        );
+      children: <Widget>[
+        Expanded(
+          flex: 6,
+          child: Container(
+            margin: EdgeInsets.only(left: 22),
+            height: 5,
+            child: Slider(
+                value: currentProgress,
+                onChanged: (value) {
+                  setState(() {
+                    currentProgress = value;
+                  });
+                  if(widget.onProgressChanged != null){
+                    widget.onProgressChanged(value);
+                  }
+                }),
+          ),
+        ),
+        SizedBox(
+          width: 20,
+        ),
+        Expanded(
+          flex: 2,
+          child: Text(
+            "${(currentProgress * 100).toInt()}%",
+            style: TextStyle(fontSize: 8),
+          ),
+        ),
+      ],
+    );
   }
 }
+
+typedef TheProgress = void Function(double value);
--- lib/pages/task_detail_page.dart
+++ lib/pages/task_detail_page.dart
@@ -13,70 +13,77 @@ class TaskDetailPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final model = Provider.of<TaskDetailPageModel>(context);
 
-    return Stack(
-      children: <Widget>[
-        Hero(
-          tag: "task_bg${index}",
-          child: Container(
-              decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(15.0),
-          )),
-        ),
-        Scaffold(
-          backgroundColor: Colors.transparent,
-          appBar: AppBar(
-            iconTheme: IconThemeData(color: Theme.of(context).primaryColor),
-            leading: IconButton(icon: Icon(Icons.arrow_back_ios), onPressed: (){
-              model.isExisting = true;
-              model.refresh();
-              Navigator.of(context).pop();
-            }),
-            elevation: 0,
+    return WillPopScope(
+      onWillPop: (){
+        model.isExisting = true;
+        model.refresh();
+        Navigator.of(context).pop();
+      },
+      child: Stack(
+        children: <Widget>[
+          Hero(
+            tag: "task_bg${index}",
+            child: Container(
+                decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(15.0),
+            )),
+          ),
+          Scaffold(
             backgroundColor: Colors.transparent,
-            actions: <Widget>[
-              IconButton(
-                  icon: Hero(
-                      tag: "task_more${index}",
-                      child: Icon(
-                        Icons.more_vert,
-                        color: Theme.of(context).primaryColor,
-                      )),
-                  onPressed: null)
-            ],
+            appBar: AppBar(
+              iconTheme: IconThemeData(color: Theme.of(context).primaryColor),
+              leading: IconButton(icon: Icon(Icons.arrow_back_ios), onPressed: (){
+                model.isExisting = true;
+                model.refresh();
+                Navigator.of(context).pop();
+              }),
+              elevation: 0,
+              backgroundColor: Colors.transparent,
+              actions: <Widget>[
+                IconButton(
+                    icon: Hero(
+                        tag: "task_more${index}",
+                        child: Icon(
+                          Icons.more_vert,
+                          color: Theme.of(context).primaryColor,
+                        )),
+                    onPressed: null)
+              ],
+            ),
+            body: Column(
+              children: <Widget>[
+                Container(
+                    margin: EdgeInsets.only(left: 50, top: 20, right: 50),
+                    child: TaskInfoWidget(index)),
+                Expanded(
+                  child: Container(
+                    margin:
+                        EdgeInsets.only(left: 50, top: 20, right: 50, bottom: 20),
+                    child: !model.isExisting? ListView(
+                      children: <Widget>[
+                        TaskDetailItem(
+                          itemName: "Journey to the West",
+                          index: 0,
+                        ),TaskDetailItem(
+                          itemName: "Romance of the Three",
+                          index: 1,
+                        ),TaskDetailItem(
+                          itemName: "Water Margin",
+                          index: 2,
+                        ),TaskDetailItem(
+                          itemName: "Joke",
+                          index: 3,
+                        ),
+                      ],
+                    ):SizedBox(),
+                  ),
+                )
+              ],
+            ),
           ),
-          body: Column(
-            children: <Widget>[
-              Container(
-                  margin: EdgeInsets.only(left: 50, top: 20, right: 50),
-                  child: TaskInfoWidget(index)),
-              Expanded(
-                child: Container(
-                  margin:
-                      EdgeInsets.only(left: 50, top: 20, right: 50, bottom: 20),
-                  child: !model.isExisting? ListView(
-                    children: <Widget>[
-                      TaskDetailItem(
-                        itemName: "Journey to the West",
-                        index: 0,
-                      ),TaskDetailItem(
-                        itemName: "Romance of the Three",
-                        index: 1,
-                      ),TaskDetailItem(
-                        itemName: "Water Margin",
-                        index: 2,
-                      ),TaskDetailItem(
-                        itemName: "Joke",
-                        index: 3,
-                      ),
-                    ],
-                  ):SizedBox(),
-                ),
-              )
-            ],
-          ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
