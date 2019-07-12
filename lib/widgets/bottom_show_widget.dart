96fbf496495141c1abdbd69fde0b3cdf7254e9a5
oldchen <772565130@qq.com>
2019-07-12 09:45:36 +0800

    解决一点bug

--- lib/widgets/bottom_show_widget.dart
+++ lib/widgets/bottom_show_widget.dart
@@ -24,10 +24,9 @@ class _BottomShowWidgetState extends State<BottomShowWidget>
   AnimationController _controller;
   Animation<double> _animation;
   List<TaskIcon> _children = [
-    TaskIcon(MyThemeColor.defaultColor, "default", Icons.add),
     TaskIcon(MyThemeColor.coffeeColor, "radio", Icons.chrome_reader_mode),
     TaskIcon(MyThemeColor.cyanColor, "game", Icons.videogame_asset),
-    TaskIcon(MyThemeColor.purpleColor, "read", Icons.book),
+    TaskIcon(MyThemeColor.defaultColor, "read", Icons.book),
     TaskIcon(MyThemeColor.greenColor, "sports", Icons.directions_run),
     TaskIcon(MyThemeColor.darkColor, "drive", Icons.drive_eta),
     TaskIcon(MyThemeColor.blueGrayColor, "work", Icons.work),
@@ -55,101 +54,99 @@ class _BottomShowWidgetState extends State<BottomShowWidget>
     final size = MediaQuery.of(context).size;
     final globalModel = Provider.of<GlobalModel>(context);
 
-    return GestureDetector(
-      onTap: () {
+    return WillPopScope(
+      onWillPop: (){
         doExit(context, _controller);
       },
-      child: Scaffold(
-        backgroundColor: Colors.black.withOpacity(0),
-        body: Container(
-          width: size.width,
-          height: size.height,
-          child: Stack(
-            children: <Widget>[
-              Positioned(
-                bottom: 20,
-                left: size.width / 2 - 28,
-                child: AnimatedBuilder(
-                    animation: _animation,
-                    child: Container(
-                      width: 56,
-                      height: 56,
-                      decoration: BoxDecoration(
-                          color: Colors.grey.withOpacity(0.5),
-                          shape: BoxShape.circle),
-                    ),
-                    builder: (ctx, child) {
-                      return Transform.scale(
-                        scale: (size.height / 28) * (_animation.value),
-                        child: child,
-                      );
-                    }),
-              ),
-              AnimatedBuilder(
-                animation: _animation,
-                child: CircleList(
-                  initialAngle: -pi / 2,
-                  origin: Offset(0, -size.width / 2 + 20),
-                  showInitialAnimation: true,
-                  children: List.generate(_children.length, (index) {
-                    return GestureDetector(
-                      onTap: (){
-                        debugPrint("点击：${_children[index].taskName}");
-                        doExit(context, _controller);
-                        Navigator.of(context).push(new CupertinoPageRoute(builder: (ctx){
-                         return ProviderConfig.getInstance().getEditTaskPage(_children[index],mainPageModel: globalModel.mainPageModel,);
-                        }));
-
-                      },
+      child: GestureDetector(
+        onTap: () {
+          doExit(context, _controller);
+        },
+        child: Scaffold(
+          backgroundColor: Colors.black.withOpacity(0),
+          body: Container(
+            width: size.width,
+            height: size.height,
+            child: Stack(
+              children: <Widget>[
+                Positioned(
+                  bottom: 20,
+                  left: size.width / 2 - 28,
+                  child: AnimatedBuilder(
+                      animation: _animation,
                       child: Container(
-                        padding: EdgeInsets.all(10),
+                        width: 56,
+                        height: 56,
                         decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            border: Border.all(
-                                color: _children[index].color, width: 1)),
+                            color: Theme.of(context).primaryColorDark.withOpacity(0.2),
+                            shape: BoxShape.circle),
+                      ),
+                      builder: (ctx, child) {
+                        return Transform.scale(
+                          scale: (size.height / 28) * (_animation.value),
+                          child: child,
+                        );
+                      }),
+                ),
+                AnimatedBuilder(
+                  animation: _animation,
+                  child: CircleList(
+                    initialAngle: -pi / 2,
+                    origin: Offset(0, -size.width / 2 + 20),
+                    showInitialAnimation: true,
+                    children: List.generate(_children.length, (index) {
+                      return GestureDetector(
+                        onTap: (){
+                          debugPrint("点击：${_children[index].taskName}");
+                          doExit(context, _controller);
+                          Navigator.of(context).push(new CupertinoPageRoute(builder: (ctx){
+                           return ProviderConfig.getInstance().getEditTaskPage(_children[index],mainPageModel: globalModel.mainPageModel,);
+                          }));
+
+                        },
                         child: Icon(
                           _children[index].iconData,
                           size: 40,
                           color: _children[index].color,
                         ),
-                      ),
-                    );
-                  }),
-                  innerCircleColor: Theme.of(context).primaryColorLight,
-                  outerCircleColor: globalModel.logic.getBgInDark(),
-                  centerWidget: GestureDetector(
-                      onTap: () {
-                        doExit(context, _controller);
-                        debugPrint("点击");
-                      },
-                      child: Container(
-                        width: MediaQuery.of(context).size.width / 3,
-                        height: MediaQuery.of(context).size.width / 3,
-                        decoration: BoxDecoration(
-                          color: Colors.black54,
-                          shape: BoxShape.circle,
-                        ),
+                      );
+                    }),
+                    innerCircleColor: Theme.of(context).primaryColorLight,
+                    outerCircleColor: globalModel.logic.getBgInDark(),
+                    centerWidget: GestureDetector(
+                        onTap: () {
+                          doExit(context, _controller);
+                          debugPrint("点击");
+                        },
                         child: Container(
-                          color: Colors.transparent,
-                          child: Icon(
-                            Icons.keyboard_arrow_down,
-                            color: Colors.white,
-                            size: 50,
+                          width: MediaQuery.of(context).size.width / 3,
+                          height: MediaQuery.of(context).size.width / 3,
+                          decoration: BoxDecoration(
+                            color: Colors.black54,
+                            shape: BoxShape.circle,
                           ),
-                        ),
-                      )),
+                          child: Container(
+                            color: Colors.transparent,
+                            child: Icon(
+                              Icons.keyboard_arrow_down,
+                              color: Colors.white,
+                              size: 50,
+                            ),
+                          ),
+                        )),
+                  ),
+                  builder: (ctx, child) {
+                    return Transform.translate(
+                        offset: Offset(
+                            0,
+                            MediaQuery.of(context).size.height -
+                                (_animation.value) * size.width),
+                        child: Transform.scale(
+                            scale: _animation.value, child: child));
+                  },
                 ),
-                builder: (ctx, child) {
-                  return Transform.translate(
-                      offset: Offset(
-                          0,
-                          MediaQuery.of(context).size.height -
-                              (_animation.value) * size.width),
-                      child: Transform.scale(
-                          scale: _animation.value, child: child));
-                },
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
